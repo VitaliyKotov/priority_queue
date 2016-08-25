4589f436843913c98817ec3561aeb184692e1f6a
Vitaliy Kotov <vkotov1@gmail.com>
2016-08-25 23:33:40 +0300

    max-heap draft

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const MaxHeap = require('./src/max-heap');
+const Node = require('./src/node');
 
 const h = new MaxHeap();
+const a = new Node(0,0);
+const b = new Node(1,1);
+const c = new Node(2,2);
+const d = new Node(3,3);
+const e = new Node(4,4);
+const f = new Node(5,5);
+
+window.a = a;
+window.b = b;
+window.c = c;
+window.d = d;
+window.e = e;
+window.f = f;
 window.h = h;
--- src/max-heap.js
+++ src/max-heap.js
@@ -2,7 +2,9 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.length = 0;
 	}
 
 	push(data, priority) {
@@ -22,7 +24,7 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		return this.length;
 	}
 
 	isEmpty() {
@@ -34,11 +36,40 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		this.length++;
+		if(!this.root) {
+			this.root = node;
+			this.parentNodes.push(node);
+			return;
+		}
+
+		if (this.parentNodes[0].left === null){
+        	this.parentNodes[0].appendChild(node);
+        	this.parentNodes.push(node);        	
+		} else {
+			this.parentNodes[0].appendChild(node);
+        	this.parentNodes.push(node);
+        	this.parentNodes.shift();
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+		let n = this.parentNodes.indexOf(node); // index of selected node in array
+		let shifted = this.parentNodes[n]; //save the selected node 
+		while(n > 0) { 
+			let parentIndex = Math.floor((n+1)/2)- 1,
+				parent = this.parentNodes[parentIndex];
+
+				if(shifted.priority > parent.priority) { 
+					shifted.swapWithParent();
+					// this.parentNodes[parentIndex] = shifted; // swap the elements if the parent is greater.
+					// this.parentNodes[n] = parent;
+					n = parentIndex; //update `n` to continue at the new position.
+				} else {
+					break; //if parent less, no need to swap
+				}
+				return this;
+		}
 	}
 
 	shiftNodeDown(node) {
