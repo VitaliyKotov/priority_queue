22037bae5850541e62107264b3966fd8ea97eacf
Vitaliy Kotov <vkotov1@gmail.com>
2016-09-14 23:35:15 +0300

    jsFormated

--- src/node.js
+++ src/node.js
@@ -8,13 +8,13 @@ class Node {
 	}
 
 	appendChild(node) {
-		if(!this.left) {
+		if (!this.left) {
 			this.left = node;
 			node.parent = this;
 			return;
 		}
-		
-		if(!this.right) {
+
+		if (!this.right) {
 			this.right = node;
 			node.parent = this;
 			return;
@@ -22,24 +22,24 @@ class Node {
 	}
 
 	removeChild(node) {
-		if(this.left == node) {
+		if (this.left === node) {
 			this.left = null;
 			node.parent = null;
 			return;
 		}
 
-		if(this.right == node) {
+		if (this.right === node) {
 			this.right = null;
 			node.parent = null;
 			return;
 		}
-		 if(!(this.right == node) && !(this.left == node)) {
-		 	throw new Error ('passed node is not a child of this node');
-		 }
+		if (!(this.right === node) && !(this.left === node)) {
+			throw new Error('passed node is not a child of this node');
+		}
 	}
 
 	remove() {
-		if(this.parent === null) {
+		if (this.parent === null) {
 			return;
 		} else {
 			this.parent.removeChild(this);
@@ -47,66 +47,68 @@ class Node {
 	}
 
 	swapWithParent() {
-		if(this.parent === null) {return}
+		if (this.parent === null) {
+			return
+		}
 		let parent = this.parent,
 			parentOfParent = parent.parent,
 			leftChild = this.left,
 			rightChild = this.right;
 
-		if(parentOfParent) { 
-			if(parentOfParent.left == parent) {
+		if (parentOfParent) {
+			if (parentOfParent.left == parent) {
 				parentOfParent.left = this;
 			} else {
 				parentOfParent.right = this;
 			}
 		}
-		if(this.parent) {
-			if(this.parent.left == this) {
+		if (this.parent) {
+			if (this.parent.left == this) {
 				this.parent.parent = this;
-				
-				if(this.parent.right) { 
+
+				if (this.parent.right) {
 					this.parent.right.parent = this;
 					this.right = this.parent.right;
 				}
 
-				if(leftChild) {
-				leftChild.parent = this.parent;
-				this.parent.left = leftChild;
-				} else {
-					this.parent.left = null;
-				}
-
-				if(rightChild) {
-					rightChild.parent = this.parent;
-					this.parent.right = rightChild;
-				} else {
-					this.parent.right = null; 
-				}
-
-				this.left = this.parent;
-				this.parent = parentOfParent;
-				
-			} else {
-				this.parent.parent = this;
-				
-				if(this.parent.left) {
-					this.parent.left.parent = this;
-					this.left = this.parent.left;
-				}
-				if(leftChild) {
+				if (leftChild) {
 					leftChild.parent = this.parent;
 					this.parent.left = leftChild;
 				} else {
 					this.parent.left = null;
 				}
 
-				if(rightChild) {
+				if (rightChild) {
 					rightChild.parent = this.parent;
 					this.parent.right = rightChild;
 				} else {
-					this.parent.right = null; 
+					this.parent.right = null;
 				}
-				
+
+				this.left = this.parent;
+				this.parent = parentOfParent;
+
+			} else {
+				this.parent.parent = this;
+
+				if (this.parent.left) {
+					this.parent.left.parent = this;
+					this.left = this.parent.left;
+				}
+				if (leftChild) {
+					leftChild.parent = this.parent;
+					this.parent.left = leftChild;
+				} else {
+					this.parent.left = null;
+				}
+
+				if (rightChild) {
+					rightChild.parent = this.parent;
+					this.parent.right = rightChild;
+				} else {
+					this.parent.right = null;
+				}
+
 				this.right = this.parent;
 				this.parent = parentOfParent;
 			}
--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,30 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.size() === this.maxSize) {
+			throw new Error("Max size of queue is reached");
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error("Queue is empty")
+		}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
