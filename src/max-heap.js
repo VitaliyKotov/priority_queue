1f5f2ef36bfa9865362152a7436a7be80aa19fb1
Vitaliy Kotov <vkotov1@gmail.com>
2016-09-14 23:34:24 +0300

    fix insertNode method

--- src/max-heap.js
+++ src/max-heap.js
@@ -9,27 +9,62 @@ class MaxHeap {
 
 	push(data, priority) {
 		
+		const node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+
+		if (!this.root) {
+			return;
+		}
+
+		const node = this.detachRoot();
+		this.restoreRootFromLastInsertedNode(node);
+		this.shiftNodeDown(this.root);
+		return node.data;
 	}
 
 	detachRoot() {
+		this.length--;
+		const detachedRoot = this.root;
 
-		let detachedRoot = this.root;
-		
-		if(this.parentNodes[0] == detachedRoot) {
+		if (this.parentNodes[0] === detachedRoot) {
 			this.parentNodes.shift();
 		}
 
 		this.root = null;
+
 		return detachedRoot;
-		
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+		const lastInsertedNode = this.parentNodes.pop();
+
+		if (!detached) {
+			return;
+		}
+
+		if (lastInsertedNode) {
+			const parent = lastInsertedNode.parent;
+			this.root = lastInsertedNode;
+			if (parent) {
+				lastInsertedNode.remove();
+				if (!parent.right && parent.left && parent !== detached) {
+					this.parentNodes.unshift(parent);
+				}
+			}
+
+			if (detached.left) {
+				lastInsertedNode.appendChild(detached.left);
+			}
+			if (detached.right) {
+				lastInsertedNode.appendChild(detached.right);
+				return;
+			}
+			this.parentNodes.unshift(lastInsertedNode);
+		}	
 	}
 
 	size() {
@@ -37,46 +72,53 @@ class MaxHeap {
 	}
 
 	isEmpty() {
-		
+		return (this.length === 0) ? true : false;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.length = 0;
 	}
 
 	insertNode(node) {
 		this.length++;
-		if(!this.root) {
+		if (!this.root) {
 			this.root = node;
 			this.parentNodes.push(node);
 			return;
 		}
 
-		if (this.parentNodes[0].left === null){
-        	this.parentNodes[0].appendChild(node);
-        	this.parentNodes.push(node);        	
+		if (this.parentNodes[0].left === null) {
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
 		} else {
 			this.parentNodes[0].appendChild(node);
-        	this.parentNodes.push(node);
-        	this.parentNodes.shift();
+			this.parentNodes.push(node);
+			this.parentNodes.shift();
 		}
-		
 	}
 
 	shiftNodeUp(node) {
 
-		let parent = node.parent;
-		let nodeIndex = this.parentNodes.indexOf(node);
-		let parentIndex = this.parentNodes.indexOf(parent);
-		if(parent && parent.priority < node.priority) {
-			this.parentNodes[parentIndex] = node;
-			this.parentNodes[nodeIndex] = parent;
+		const parent = node.parent;
+
+		if (parent && parent.priority < node.priority) {
+			if (parent === this.root) {
+				this.root = node;
+			}
+			const nodeIndex = this.parentNodes.indexOf(node);
+			const parentIndex = this.parentNodes.indexOf(parent);
+			if (parentIndex === -1) {
+				this.parentNodes[nodeIndex] = parent;
+			} else {
+				this.parentNodes[parentIndex] = node;
+				this.parentNodes[nodeIndex] = parent;
+			}
+
 			node.swapWithParent();
 			this.shiftNodeUp(node);
-		} else {
-			this.root = node;
 		}
-			
 	}
 
 	shiftNodeDown(node) {
@@ -85,13 +127,13 @@ class MaxHeap {
 		}
 
 		if (node.left) {
-			var childToShift = node.left;
+			let childToShift = node.left;
 			if (node.right && childToShift.priority <= node.right.priority) { // check if node has right child and if its priority greater than left's child
 				childToShift = node.right; // if so we will move right, to ensure greater priority to be higher
 			}
 			if (childToShift.priority > node.priority) {
-				let childToShiftIndex = this.parentNodes.indexOf(childToShift);
-				let nodeIndex = this.parentNodes.indexOf(node);
+				const childToShiftIndex = this.parentNodes.indexOf(childToShift);
+				const nodeIndex = this.parentNodes.indexOf(node);
 				if (node === this.root) {
 					this.root = childToShift;
 				}
@@ -103,14 +145,9 @@ class MaxHeap {
 				}
 				childToShift.swapWithParent();
 				this.shiftNodeDown(node);
-
 			}
-
 		}
-
 	}
-
 }
 		
-
 module.exports = MaxHeap;
